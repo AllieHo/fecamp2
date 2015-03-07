860ba09f424bab76994a39172ec553fedf97fb54
AllieHo <youngsung.ho@gmail.com>
2015-03-07 14:37:28 +0900

    test

---
+++ html/default.js
@@ -0,0 +1,38 @@
+jQuery(function($){
+	// 모바일 GNB 토글 버튼
+	var $gnbBtn = $(".gnb>button");
+
+	$gnbBtn.on("click", function(){
+		$(this).next().slideToggle(200);
+
+		if( $gnbBtn.text() === "메뉴 열기" ){
+			//메뉴 닫기 텍스트 표시
+			$gnbBtn.text("메뉴 닫기");
+		} else {
+			//메뉴 열기 텍스트 표시
+			$gnbBtn.text("메뉴 열기");
+		}
+	});
+
+	// 태블릿 + 데스크톱 GNB 메뉴
+	$(".gnb>ul>li>a").on("mouseover focus", function(){
+		$(this).parent().addClass("active").siblings().removeClass("active");
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
